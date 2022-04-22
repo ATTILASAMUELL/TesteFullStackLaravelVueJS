721ca6ae474a671d2c0ef65dd9efbf8f53d8bc9f
Kevin Souza <kevin.souza@devsquad.com>
2022-04-22 19:37:20 -0300

    using english language and new database table

--- apiLaravel/database/migrations/2022_04_19_015415_create_registers_table.php
+++ apiLaravel/database/migrations/2022_04_19_015415_create_registers_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateRegistrosTable extends Migration
+class CreateRegistersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,14 @@ class CreateRegistrosTable extends Migration
      */
     public function up()
     {
-        Schema::create('registros', function (Blueprint $table) {
-            //
+        Schema::create('registers', function (Blueprint $table) {
+            
             $table->id();
-            $table->string('nome');
+            $table->string('name');
             $table->string('email')->unique();
-            $table->string('senha');
-            $table->string('telefone');
-            $table->string('foto');
-
+            $table->string('password');
+            $table->string('phone');
+            $table->string('picture');
         });
     }
 
@@ -32,6 +31,6 @@ class CreateRegistrosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('registros');
+        Schema::dropIfExists('registers');
     }
 }
--- apiLaravel/routes/api.php
+++ apiLaravel/routes/api.php
@@ -1,16 +1,16 @@
 <?php
 
+use App\Http\Controllers\RegisterController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegistrosController;
 
+Route::apiResource('registers', RegisterController::class);
 
-//====== CRUD Registros: 
-
-// POST  // GET  // PUT  // DELETE
-
+/*
 Route::post('/registro', [RegistrosController::class, 'criarRegistro']);
 Route::get('/registro', [RegistrosController::class, 'listarTodosRegistros']);
 //Route::get('/registro/{id}', [RegistrosController::class, 'listarRegistro']);
-Route::post('/editando',[RegistrosController::class, 'alterarRegistro']);
-Route::delete('/registro/{id}',[RegistrosController::class, 'deletandoRegistro']);
+Route::post('/editando', [RegistrosController::class, 'alterarRegistro']);
+Route::delete('/registro/{id}', [RegistrosController::class, 'deletandoRegistro']);
+ */
