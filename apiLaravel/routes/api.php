25f132fba018168607f2eb51819698430940c597
Attila tabory <atila.samuell@gmail.com>
2022-04-24 11:32:46 -0300

    bug reslvido -commit referente ao bug encontrado na parte de upload de imagem

--- apiLaravel/routes/api.php
+++ apiLaravel/routes/api.php
@@ -7,10 +7,9 @@ use App\Http\Controllers\RegistrosController;
 
 //====== CRUD Registros: 
 
-// POST  // GET  // PUT  // DELETE
 
 Route::post('/registro', [RegistrosController::class, 'criarRegistro']);
 Route::get('/registro', [RegistrosController::class, 'listarTodosRegistros']);
-//Route::get('/registro/{id}', [RegistrosController::class, 'listarRegistro']);
+Route::get('/registro/{id}', [RegistrosController::class, 'listarRegistro']);
 Route::post('/editando',[RegistrosController::class, 'alterarRegistro']);
 Route::delete('/registro/{id}',[RegistrosController::class, 'deletandoRegistro']);
